d77a43cc07927e29e36b50ee2d1686ee1abbc1cd
PatilChetan07 <112507177+PatilChetan07@users.noreply.github.com>
2022-11-06 12:54:04 +0530

    Create IO STREAM.java

---
+++ IO STREAM.java
@@ -0,0 +1,22 @@
+/* Java uses the concept of a stream to make I/O operation fast. 
+The java.io package contains all the classes required for input and output operations.
+3 streams are created for us automatically.
+1)System.out: standard output stream 2)System.in: standard input stream 3)System.err: standard error stream */
+
+// import java.io.Main;
+
+// public class Main{
+//     public static void main (String[] args) {
+//       try{
+//           Main obj = new Main("Main.txt");
+//           String s = "john";
+//           byte b [] = s.getBytes();
+//           obj.write(b);
+//           obj.close();
+//           System.out.println("success");
+//       }
+//       catch(Exception e){
+//           System.out.println(e);
+//       }
+//     }
+// }
