448765ad69086ee33b8c7c0aec2bc3c151a027d5
PatilChetan07 <112507177+PatilChetan07@users.noreply.github.com>
2022-11-07 01:08:00 +0530

    Update IO STREAM.java

--- IO STREAM.java
+++ IO STREAM.java
@@ -3,20 +3,28 @@ The java.io package contains all the classes required for input and output opera
 3 streams are created for us automatically.
 1)System.out: standard output stream 2)System.in: standard input stream 3)System.err: standard error stream */
 
-// import java.io.Main;
+import java.io.*;
 
-// public class Main{
-//     public static void main (String[] args) {
-//       try{
-//           Main obj = new Main("Main.txt");
-//           String s = "john";
-//           byte b [] = s.getBytes();
-//           obj.write(b);
-//           obj.close();
-//           System.out.println("success");
-//       }
-//       catch(Exception e){
-//           System.out.println(e);
-//       }
-//     }
-// }
+class Main{
+    public static void main(String args[]) throws IOException{
+        char c;
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        System.out.print("Enter the character : ");
+        c = (char)br.read();
+        System.out.println(c);
+
+        FileInputStream in = new FileInputStream("Input.txt");
+        FileOutputStream out = new FileOutputStream("Output.txt");
+        int a;
+        int k = 0;
+        while((a = in.read()) != -1){
+            out.write(a);
+            System.out.print((char)a);
+            k++;
+        }
+        System.out.println();
+        System.out.println("No of char copied : " + k);
+    }
+}
