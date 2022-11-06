8d4e0771fbd41ab505a10daa7cdb3e19c9d04604
PatilChetan07 <112507177+PatilChetan07@users.noreply.github.com>
2022-11-06 11:44:00 +0530

    Create PACKAGES.java

---
+++ PACKAGES.java
@@ -0,0 +1,7 @@
+/*  A java package is a group of similar types of classes, interfaces and sub-packages.
+ Package in java can be categorized in two form, built-in package and user-defined package.
+ There are many built-in packages such as java, lang, awt, javax, swing, net, io, util, sql etc.
+ Advantage -
+1) used to categorize the classes and interfaces so that they can be easily maintained. 
+2)provides access protection.
+3)removes naming collision. */
